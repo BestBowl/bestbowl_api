12b69d18aacd7366ac15b8a31dea38b9299cd303
David Macpherson <david.allan.macpherson@gmail.com>
2023-11-13 20:43:43 -0500

    first prototype designations fishished, adding subcategorys cause i can and i dont wanna work on the frontend rn

--- app.py
+++ app.py
@@ -6,13 +6,14 @@ dotenv.load_dotenv()
 import card_adder
 import datetime
 import secrets
+import base64
 import jsonpickle
 from fsrs import FSRS, Card, SchedulingInfo, ReviewLog ## this line will probably throw an error because i had to modify fsrs's __init__.py to give me the classes i need. this library is kinda dumb.
 import carder
 import flask
 import json
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask, request, redirect as r
+from flask import Flask, request, redirect as r, Response
 from flask_cors import CORS
 from functools import wraps
 
@@ -22,7 +23,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
 app.config["SECRET_KEY"] = "ENTER YOUR SECRET KEY"
 # Initialize flask-sqlalchemy extension
 db = SQLAlchemy()
- 
+INITIAL_SETTING = 1 ## hard
 
 
 dotenv.load_dotenv()
@@ -41,11 +42,45 @@ class User(db.Model):
 def stringify(obj):
     return json.dumps(obj)
 def get_default_user_data():
+    '''
+    versioning:
+    1.0.0: uses jsonpickle to turn the ScheduleInfo class into json and back. not a good solution but it works
+    '''
     return {
+        "storage_version": "1.0.0",
         "max_new_cards": 3,
         "last_added_cards": "0000-00-00",
         "cards": [] ##this will be a large array of (uuid, (int, schedulingstuff))
     }
+
+def serialize(s: any):
+    '''
+    serialize arbritary class into json object (depth not max)
+    '''
+    data = {}
+    for property in s.__dict__:
+        cardobj = s.__dict__[property].__dict__.copy()
+        for entry in cardobj:
+            if type(cardobj[entry]) == datetime.datetime:
+                cardobj[entry] = cardobj[entry].isoformat()
+        data[property] = cardobj
+    return data
+def serialize_card(s: [str, SchedulingInfo]):
+    '''
+    turn a scheduling info into a json_readable object.
+    '''
+    data = {}
+    s2 = s[1]
+    for property in s2.__dict__:
+        
+        cardobj = s2.__dict__[property].__dict__.copy()
+        for entry in cardobj:
+            if type(cardobj[entry]) == datetime.datetime:
+                cardobj[entry] = cardobj[entry].isoformat()
+        data[property] = cardobj
+    questionurl = request.host_url + "uuid_to_question?card_uuid="+s[0]
+    data["url_to_question"] = questionurl
+    return data
 def get_random_user_id():
     return secrets.token_hex()
 # Initialize app with extension
@@ -93,52 +128,58 @@ def needs_user_id(func):
 
 
 
-def get_date():
+def get_date(): 
     ## gets time mm/dd/yy
     return datetime.date.today().strftime("%Y-%m-%d")
-    
-def serialize_schedule(scheduling_stuff: dict[int, SchedulingInfo]):
-    ## arghhh i gotta do this manually
-    sched_info = scheduling_stuff.values()
-    data = {}
-    for index, sched in enumerate(sched_info):
-        card = sched.card
-        review_log = sched.review_log
-        ##first card
-        card_dict = card.__dict__
-        card_dict['due'] = datetime.datetime.isoformat(card_dict['due'])
-        card_dict['last_review'] = datetime.datetime.isoformat(card_dict['last_review'])
-        ##second review
-        review_dict = review_log.__dict__
-        review_dict['review'] = datetime.datetime.isoformat(review_dict['review'])
-        card_info = [card_dict, review_dict]
-        data[index+1] = card_info
-    return data
 
-def deserialize_schedule(json_stuff: dict[int, dict]):
+def get_user_cards(user):
+    userinfo = json.loads(user.data)
+    return [[a[0], save_deserialize_schedule(a[1])] for a in userinfo['cards']]
+def put_back_user_cards(user, cards):
+    serialized_cards = [[a[0], save_serialize_schedule(a[1])] for a in cards]
+    userinfo = json.loads(user.data)
+    userinfo['cards'] = serialized_cards
+    user.data = json.dumps(userinfo)
+    db.session.commit()
     
-    data = {}
-    for index, sched in enumerate(json_stuff.values()):
-        card_dict = sched[0]
-        # print(card_dict)
-        card_dict['due'] = datetime.datetime.fromisoformat(card_dict['due'])
-        card_dict['last_review'] = datetime.datetime.fromisoformat(card_dict['last_review'])
-        review_dict = sched[1]
-        review_dict['review'] = datetime.datetime.fromisoformat(review_dict['review'])
-        # print(review_dict)
-        new_card = Card() 
-        ## first card
-        for key in card_dict:
-            setattr(new_card, key, card_dict[key])
-        review_log = ReviewLog(*review_dict) ## ORDER MATTERS
-        sched_info = SchedulingInfo(new_card, review_log)
-        
-        # print(sched_info)
-        # print(review_log)
-        # print(new_card)
-        data[index+1] = sched_info ## zero is there for equality purposes
-    return data
+def save_serialize_schedule(card: SchedulingInfo):
+    return json.loads(jsonpickle.encode(card, unpicklable=True))       
+
+def save_deserialize_schedule(card: dict):
+    return jsonpickle.decode(json.dumps(card), classes=(SchedulingInfo, Card, ReviewLog)) ##efficency at it's finest
     
+def get_user_card_by_uuid(user: User, card_uuid: str, cards=None) -> [str, SchedulingInfo]:
+    if (cards == None):
+        all_cards = get_user_cards(user)
+    else:
+        all_cards = cards
+    _cards = list(filter(lambda c: c[0] == card_uuid, all_cards))
+    if len(_cards) == 0:
+        raise ValueError("Card UUID is either not in user's deck or not a valid UUID.")
+    return _cards[0]
+
+def get_due_date_from_sched(sched: SchedulingInfo):
+    return sched.card.due
+
+
+def add_card(card: Card, userdata: dict):
+    '''
+    adds a card to the srs database
+    '''
+ 
+    uuid = str(card["metadata"]["uuid"])
+    ## check if it exists
+    new_fsrs_card = Card()
+    scheduled_stuff = F.repeat(new_fsrs_card, datetime.datetime.now())
+    same_cards = list(filter(lambda x: x[0] == uuid, userdata['cards']))
+    if len(same_cards) > 0:
+        raise KeyError("Card already present in DB")
+    card = scheduled_stuff[INITIAL_SETTING]
+    # srs_card = card
+    userdata['cards'].append((uuid, save_serialize_schedule(card)))
+    return card
+   
+
 
 def updateUserData(user: User):
     '''
@@ -150,14 +191,7 @@ def updateUserData(user: User):
     if userdata['last_added_cards'] != get_date():
         for _ in range(userdata['max_new_cards']):
             new_card = carder.get_random_card()
-            uuid = str(new_card.metadata.uuid)
-            ## check if it exists
-            new_fsrs_card = Card()
-            scheduled_stuff = F.repeat(new_fsrs_card, datetime.datetime.now())
-            same_cards = list(filter(lambda x: x[0] == uuid, userdata['cards']))
-            if len(same_cards) > 0:
-                continue
-            userdata['cards'].append((uuid, serialize_schedule(scheduled_stuff)))
+            add_card(new_card, userdata)
         userdata['last_added_cards'] = get_date()
     # print(userdata)
     user.data = json.dumps(userdata)
@@ -181,29 +215,66 @@ def version():
     return message.format(version_info=version_info)
 
 
-@app.route("/get_near_cards")
-# @needs_user_id()
-@needs_params(["word", "limit"])
-def get_near_cards(word, limit, user=None):
-    results = carder.get_near_cards(word, limit)
+@app.route("/search_cards")
+@needs_params(["query", "limit"])
+def search_cards(query=None, limit=None, user=None):
+    results = carder.get_near_card(query, limit)
     return results
 
-@app.route("/card_repetition")
+@app.route("/practice_card")
 @needs_user_id
 @needs_params(["rating", "card_uuid"])
-def card_repetition(rating, card_uuid, user=None):
+def practice_card(rating=None, card_uuid=None, user=None):
         rating = int(rating)
-        userinfo = json.loads(user.data)
-        all_cards = [[a[0], deserialize_schedule(a[1])] for a in userinfo['cards']]
-        card = list(filter(lambda c: c[0] == card_uuid, all_cards))[0]
-        print("found card: " + repr(card)) 
-        if (rating not in range(1, 4)):
+        if (rating not in range(1, 5)):
             raise ValueError("Rating must be integer in range [1,4], inclusive")
         
+        all_cards = get_user_cards(user)
+        card = get_user_card_by_uuid(user, card_uuid, cards=all_cards)
+        scheduling_info: SchedulingInfo = card[1]
+        scheduling_perms = F.repeat(scheduling_info.card, datetime.datetime.now())
+        card[1].card = scheduling_perms[rating].card
+        # print(all_cards[0q][1].card.__dict__)
+        put_back_user_cards(user, all_cards)
+        return Response("done", status=201)
+
+@app.route("/view_cards") 
+@needs_user_id
+@needs_params(["due"], optional=True)
+def view_due_cards(user=None, due=None):
+    all_cards = get_user_cards(user)
+    if due == "true":
+        # dates = map(get_due_date_from_sched, all_cards)
+        due_dates = list(map(serialize_card,filter(lambda sched: sched[1].card.due <= datetime.datetime.now(), all_cards)))
+        return due_dates
+    return list(map(serialize_card, all_cards))
+
+
+@app.route("/get_random_question")
+@needs_user_id
+def get_random_question(user=None):
+    '''
+    gets a random new card from weaviate and adds it to the user's deck, and returns the question.
+    '''
+    userdata = json.loads(user.data)
+    weaviate_card = carder.get_random_card()
+    srs_card = add_card(weaviate_card, userdata)
+    card = {**weaviate_card, "srs": serialize(srs_card)}
+    return card
+
+
+@app.route("/uuid_to_question")
+@needs_user_id
+@needs_params(["card_uuid"])
+def uuid_to_question(card_uuid=None, user=None):
+    question = carder.get_card_by_uuid(card_uuid)
+    srs_card = get_user_card_by_uuid(user, card_uuid)
+    card = {**question, "srs": serialize(srs_card)}
+    return card
         
 @app.route("/create_user")
-@optional_params(["redirect"])
-def create_user(redirect):
+@needs_params(["redirect"], optional=True)
+def create_user(redirect=None):
     ### if someone doesnt have a user id, this makes them a new user
     new_user = User(user_id=get_random_user_id(), data=stringify(get_default_user_data()))
     db.session.add(new_user)
--- carder.py
+++ carder.py
@@ -5,7 +5,10 @@ import weaviate
 import weaviate.classes as wvc
 import os
 import random
+from dataclasses import asdict
+from functools import wraps
 import dotenv
+import json
 dotenv.load_dotenv()
 client = weaviate.connect_to_local(
     port=8080,
@@ -17,21 +20,96 @@ client = weaviate.connect_to_local(
 question = client.collections.get("Question")
 database = [a for a in question.iterator()]
 
-def get_near_card(word, limit):
+with open("./subcategories.json", mode="r", encoding="utf-8") as f:
+    subcategories = json.load(f) ###AHHH THE MEMORY REQUIREMENTS GO CRAZYYYYYY
+with open("./card_adder/filtered.json", mode="r", encoding="utf-8") as f:
+    filtered = json.load(f)
+        
+def get_keys_of(cls):
+    return [a for a in dir(cls) if not a.startswith('_') and not callable(getattr(cls, a))]
+def to_dict(obj):
+    '''
+    converts arbritary _object to dictionary
+    '''
+    # data = {}
+    # def _recursive(obj):
+    #     keys = get_keys_of(obj)
+    #     print(keys)
+    #     for key in keys:
+    #         data[key] = obj.__dict__[key]
+    #         if hasattr(data[key], "__dict__"):
+    #             _recursive(data[key])
+    # _recursive(obj)
+    # # print(data)
+    # return data
+    return asdict(obj=obj)
+            
+def subcategory(func):
+    @wraps(func)
+    def wrapper(*args, subcategories=[], **kwargs):
+        ## assumes that the function it is wrapping returns either an array of weaviate cards or a weaviate card
+        result = func(*args, **kwargs)
+        def _filter_by_subcategories():
+            # return [a for a in result if a] #TODO
+            pass
+        if type(result) == list:
+            return list(map(_filter_by_subcategories, result))
+        else:
+            return _filter_by_subcategories(result)
+    return wrapper
+def extra_info(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        ## assumes that the function it is wrapping returns either an array of weaviate cards or a weaviate card
+        result = func(*args, **kwargs)
+        def _get_extra_info(card): 
+            properties = card['properties']
+            nid = properties['nid']
+            if nid[0] == "n":
+                ## this means the card was sourced from nocard.
+                id = nid[1:]
+                
+                # print(id)
+                card_match = list(filter(lambda x: str(x["id"]) == id, filtered))
+                if len(card_match) == 0:
+                    print(f"No match found for {str(id)}")
+                    return result
+                card_match = card_match[0]
+                del card_match["text"]
+                del card_match["answer"]
+                del card_match["formatted_answer"]
+                del card_match["formatted_text"]
+               
+                card["extra"] = card_match
+                
+                # print(result.__dict__)
+                print(card_match)
+                return card
+        if type(result) == list:
+            return list(map(_get_extra_info, result))
+        else:
+            return _get_extra_info(result)
+    return wrapper
+def _get_near_card(word, limit):
     q = client.collections.get("Question")
     limit = int(limit)
     if (limit > 50):
         limit = 50
     similar = q.query.near_text(query=word,limit=limit)
-    results = [obj for obj in similar.objects]
+    results = [to_dict(obj) for obj in similar.objects]
     return results
 
+get_near_card = extra_info(_get_near_card)
 def get_card_by_uuid(uuid):
-    return client.collections.get("Question").query.fetch_object_by_id(uuid=uuid, include_vector=True)
-def get_random_card():
+    return to_dict(client.collections.get("Question").query.fetch_object_by_id(uuid=uuid, include_vector=True))
+
+
+def _get_random_card():
     '''
     returns a random card from weaviate
     '''
     rand = random.randint(0, len(database))
     query_result = database[rand] ## wish i could do this with weaviate but its actually rlly hard to just get a random entry without loading the entire fricking database into memory. argh.
-    return query_result
+    return to_dict(query_result)
+
+get_random_card = extra_info(_get_random_card)
